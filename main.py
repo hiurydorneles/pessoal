3cd3a85fbcd974dde446085e2a132a16cba61903
hiurydorneles <hiury_dorneles@estudante.sesisenai.org.br>
2025-05-14 14:19:01 +0000

    update

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request
+import os 
+
+#define a pasta onde estão os arquivos html
+#(neste caso, a raiz do projeto)
+template_dir = os.path.abspath(os.path.dirname(__file__))
+app = Flask(__name__, template_folder=template_dir)
+
+#direcionamento pagina index HTML
+@app.route("/")
+def home ():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(host= "0.0.0.0", port=3000)
